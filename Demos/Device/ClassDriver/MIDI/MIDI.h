407ecd2fcbe54141aed1a84e5dd492886c9e1a42
Elijah Insua <tmpvar@gmail.com>
2014-10-03 23:49:07 -0700

    begin fleshing out single button midi sender

--- Demos/Device/ClassDriver/MIDI/MIDI.h
+++ Demos/Device/ClassDriver/MIDI/MIDI.h
@@ -67,7 +67,6 @@
 
 	/* Function Prototypes: */
 		void SetupHardware(void);
-		void CheckJoystickMovement(void);
 
 		void EVENT_USB_Device_Connect(void);
 		void EVENT_USB_Device_Disconnect(void);
